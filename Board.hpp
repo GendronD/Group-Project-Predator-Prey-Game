b38e3aa4374565301dc1efa7bb7f7ecf451eed25
Devin <gendron.devin@gmail.com>
2018-02-08 21:01:22 -0500

    Update Board.hpp

--- Board.hpp
+++ Board.hpp
@@ -1,1 +1,28 @@
+Board Header File:
+#ifndef BOARD_HPP
+#define BOARD_HPP
 
+Class Board
+{
+	Private:
+		//(if we want extra credit, use these)
+		int rows = 0;
+		int cols = 0;
+		const int boardRows = 20;
+		const int boardCols = 20;
+char **board;	
+	Public:
+    Board();  //default
+    Board(int, int);  //overloard
+    ~Board();  //destructor
+		Void createBoard();
+		Void printBoard();
+		//(extra credit)
+		Void setRows(int r);	//could use my inputval function for these -DG
+		Void setColumns(int c);
+
+		//gonna need this at some point?
+		int getRandom(int min, int max);
+};
+
+#endif
