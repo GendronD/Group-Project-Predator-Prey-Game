8f532a38de390235211f4a2790aa4f00f903f7bf
Devin <gendron.devin@gmail.com>
2018-02-08 21:17:43 -0500

    Update RunGame.cpp

--- RunGame.cpp
+++ RunGame.cpp
@@ -1,1 +1,43 @@
+// default constructor
+RunGame::RunGame()
+{
+  
+}
 
+//overload needed??
+
+//destructor
+RunGame::~RunGame()
+{
+  
+}
+
+void RunGame::simulation()
+{
+	//dynamically allocate board space
+	board = new char*[boardRows];
+	for (int i = 0; i < boardRows; i++)
+	{
+		board[i] = new char[boardCols];
+	}
+
+	//creates board
+	createBoard();
+
+	for (int i = 0; i < steps; i++)
+	{
+		//moves critters
+		moveFunc();
+
+		//prints board
+		printBoard();
+	}
+
+	//delete board
+	for (int i = 0; i < 2; i++)
+	{
+		delete [] board[i];
+	}
+	delete [] board;
+	board = NULL;
+}
