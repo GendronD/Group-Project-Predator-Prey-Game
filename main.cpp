4c6d9e1c4a8bc0725cdb28616829c83235c8b1d6
Devin <gendron.devin@gmail.com>
2018-02-08 20:51:59 -0500

    Create main.cpp
    Main file for Predator-Prey Game

---
+++ main.cpp
@@ -0,0 +1,36 @@
+Menu
+#include <iostream>
+#include *****inputval file that is chosen*******
+#include *****class files**********
+Int main ()
+{
+  Int choice = 0;
+  Std::cout << “Extra credit is implemented on this project” << std::endl;
+
+  Std::cout << “Predator Prey Menu” << std::endl;
+  Std::cout << “1. Play Set Game” << std::endl;
+  Std::cout << “2. Play Custom Game (EC)” << std::endl;
+  Choice = inputValidation();
+
+    Switch (choice)
+    {
+      Case 1 : //set game
+        Default constructor for game
+        Prompt user for number of steps
+        break;
+      Case 2 : //custom game
+        Prompt user for number of rows
+        Prompt user for number of columns
+        Prompt user for number of ants (controlled by rows/cols)
+        Prompt user for number of doodlebugs (controlled by rows/cols)
+        Prompt user for number of steps
+        Break;
+    }
+  //start game sim here
+    Do
+    {
+      game.predatorPrey();	//runs simulation
+    }while (endGame != 1);
+    
+    return 0;
+}
