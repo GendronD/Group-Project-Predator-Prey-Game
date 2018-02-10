95a3f483c9edc3bb93955fe4150cd30016be8174
Rondellio <ron.walters23@gmail.com>
2018-02-10 08:41:49 -0500

    Ant file - Ron

--- Ant.h
+++ Ant.h
@@ -1,1 +1,12 @@
+#ifndef ANT_HPP
+#define ANT_HPP
 
+class Ant: public Critter
+{
+  public:
+    Ant();
+    virtual void move(char**, int, int);
+    virtual void breed(char**, int, int);
+};
+
+#endif
