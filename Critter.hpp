91221f30263c4f27f9dd8b0480bc1bdc6795e88a
Rondellio <ron.walters23@gmail.com>
2018-02-10 09:16:38 -0500

    Critter Header - Ron

---
+++ Critter.hpp
@@ -0,0 +1,15 @@
+#ifndef CRITTER_HPP
+#define CRITTER_HPP
+
+class Critter
+{
+	protected:
+		int daysAlive;
+		int xPos;
+		int yPos;
+	private:
+		virtual void move(char**, int, int)= 0;
+		virtual void breed(char**, int, int)= 0;
+};
+
+#endif
