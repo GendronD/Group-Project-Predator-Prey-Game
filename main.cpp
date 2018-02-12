e06e78f65098766faa0f19a39a4465dd0d20686b
Sheila <sheila.babayan@gmail.com>
2018-02-11 22:54:52 -0500

    New game.cpp/hpp and updated promptUser
    Delete RunGame and inputValidation and the old promptUser

--- main.cpp
+++ main.cpp
@@ -5,47 +5,60 @@
  * Date: 2/19/2018
  * Description: This is the Main function for Group 8's group project.
  **********************************************************************/
-
-#include "inputValidation.hpp"
-#include "Critter.hpp"
-#include "Ant.hpp"
-#include "DoodleBug.hpp"
-#include "Board.hpp"
-#include "PromptUser.hpp"
-
 #include <iostream>
 using std::cout;
 using std::endl;
 
+#include "game.hpp"
+#include "promptUser.hpp"
+//#include "critter.hpp"
+//#include "ant.hpp"
+//#include "doodleBug.hpp"
+//#include "board.hpp"
+
 int main ()
-{   
+{
     srand(time(NULL));  //random seed
+    
     int choice = 0;
     int endGame = 1;
     cout << "Extra credit is implemented on this project" << endl;
+    
+    //create a Game object
+    Game g1;
+    
+    //call the Game's starting menu options
+    g1.startMenu();
+    
+    //validate the user's selection
+    choice = g1.inputValidation();
 
-    cout << "Predator Prey Menu" << endl;
-    cout << "1. Play Set Game" << endl;
-    cout << "2. Play Custom Game (EC)" << endl;
-  
-    choice = inputValidation();
-
-switch (choice)
-{
-      case 1:   //default/normal game
-                //Default constructor for game
-                promptSteps();
-                break;
-      case 2:   //custom game
-                promptRows();        //Prompt user for number of rows
-                promptCols();        //Prompt user for number of columns
-                promptAnts();        //Prompt user for number of ants (controlled by rows/cols)
-                promptDoodles();     //Prompt user for number of doodlebugs (controlled by rows/cols)
-                promptSteps();       //Prompt user for number of steps
-                break;
-      default:  cout << "That's not a menu option, something went wrong." << endl;
+    switch (choice)
+    {
+        case 1:     //Default constructor for gameboard
+                    //set the gameboard's #rows, cols
+                    //Prompt user for number of steps
+                    promptSteps();
+                    break;
+        case 2:     promptRows();        //Prompt user for number of rows
+                    promptCols();        //Prompt user for number of columns
+                    promptAnts();        //Prompt user for number of ants (controlled by rows/cols)
+                    promptDoodles();     //Prompt user for number of doodlebugs (controlled by rows/cols)
+                    promptSteps();       //Prompt user for number of steps
+                    //constructor with parameters for gameboard
+                    break;
+        default:    cout << "That's not a menu option. Please try again!\n" << endl;
+                    //start over by recursively calling main
+                    main();
     }
-  
+    
+    
+    //this is for testing, we can delete it later. Not sure how endGame works below.
+    if ((choice == 1) || (choice == 2))
+    {
+        g1.simulation();
+    }
+    /*
     //start game sim here
     do
     {
@@ -54,6 +67,7 @@ switch (choice)
         endGame = 1;
     }
     while (endGame != 1);
+    */
     
     return 0;
 }
---
+++ promptUser.cpp
@@ -0,0 +1,516 @@
+/*********************************************************************
+ * Program name: Group Project 1 - Group 8 - promptUser.cpp
+ * Authors: Sheila Babayan, Becky Chao, Elizabeth Donato, 
+ * Devin Gendron, Ronald Walters
+ * Date: 2/19/2018
+ * Description: This is the implementation file for our promptUser functions,
+ * which are used to establish the parameters of the game (rows, columns,
+ * number of bugs, steps in gameplay). It also performs input validation.
+ **********************************************************************/
+
+#include "promptUser.hpp"
+#include "game.hpp"
+
+
+int promptRows()        //Prompt user for number of rows
+{
+	int rows = 0;
+	int exitCount = 0;
+
+	//menu
+	std::cout << "Please enter the number of rows between 3 and 250." << std::endl;
+
+	do
+	{
+		exitCount = 0;			//sets to continue while loop in do-while
+		while (exitCount == 0)
+		{
+			int correctEntry = 0;
+			int incorrectEntry = 0;
+			std::string str = " ";
+			int inputLength = str.size();
+
+			//arrays to check string	
+			char numEntry[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+			
+			char letterEntry[] =
+			{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+			
+			char symEntry[] = {'`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+			'{', '}', '[', ']', '"', '<', '>', ',', '.', '?', '/', ' ', '-', '_', '+', '='};
+			//removed: '\', '|', ';', ':', ''',
+
+			//reads in user input
+			getline(std::cin, str);
+			std::stringstream ss(str);
+
+			//checks for empty strings
+			bool badEntry = false;
+			if (str.empty())
+			{
+				badEntry = true;
+			}
+
+			//checks for proper ints or incorrect entries
+			for (int t = 0; t < str.size(); t++)
+			{
+				for (int m = 0; m < 10; m++)
+				{
+					if (str[t] == numEntry[m])
+					{							
+						correctEntry ++;
+					}
+				}
+				for (int y = 0; y < 52; y++)
+				{
+					if (str[t] == letterEntry[y])
+					{										
+						incorrectEntry ++;
+					}
+				}
+				for (int n = 0; n < 28; n++)
+				{
+					if (str[t] == symEntry[n])
+					{
+						incorrectEntry ++;
+					}
+				}
+			}
+			if (badEntry == true)
+			{
+				std::cout << "Please enter at least one integer." << std::endl;
+				exitCount = 0;
+			}
+			else if (correctEntry == str.size())
+			{
+				ss >> rows;
+				//std::cout << "Correct Input." << std::endl;
+				exitCount++;
+			}
+			else if (incorrectEntry > 0)
+			{
+				std::cout << "Incorrect Entry." << std::endl;
+				std::cout << "Please enter an integer: No characters, symbols, doubles, floats, or decimals." << std::endl;
+				exitCount = 0;
+			}
+		}
+
+		if (rows >= 3 && rows <= 250)
+		{
+			exitCount = 0;
+		}
+		else
+		{
+			exitCount = 1;
+			std::cout << "Please use proper range." << std::endl;
+		}
+
+	} while(exitCount == 1);
+
+	return rows;
+}
+
+int promptCols()        //Prompt user for number of columns
+{
+	int cols = 0;
+	int exitCount = 0;
+
+	//menu
+	std::cout << "Please enter the number of columns between 3 and 250." << std::endl;
+
+	do
+	{
+		exitCount = 0;			//sets to continue while loop in do-while
+		while (exitCount == 0)
+		{
+			int correctEntry = 0;
+			int incorrectEntry = 0;
+			std::string str = " ";
+			int inputLength = str.size();
+
+			//arrays to check string	
+			char numEntry[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+			
+			char letterEntry[] =
+			{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+			
+			char symEntry[] = {'`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+			'{', '}', '[', ']', '"', '<', '>', ',', '.', '?', '/', ' ', '-', '_', '+', '='};
+			//removed: '\', '|', ';', ':', ''',
+
+			//reads in user input
+			getline(std::cin, str);
+			std::stringstream ss(str);
+
+			//checks for empty strings
+			bool badEntry = false;
+			if (str.empty())
+			{
+				badEntry = true;
+			}
+
+			//checks for proper ints or incorrect entries
+			for (int t = 0; t < str.size(); t++)
+			{
+				for (int m = 0; m < 10; m++)
+				{
+					if (str[t] == numEntry[m])
+					{							
+						correctEntry ++;
+					}
+				}
+				for (int y = 0; y < 52; y++)
+				{
+					if (str[t] == letterEntry[y])
+					{										
+						incorrectEntry ++;
+					}
+				}
+				for (int n = 0; n < 28; n++)
+				{
+					if (str[t] == symEntry[n])
+					{
+						incorrectEntry ++;
+					}
+				}
+			}
+			if (badEntry == true)
+			{
+				std::cout << "Please enter at least one integer." << std::endl;
+				exitCount = 0;
+			}
+			else if (correctEntry == str.size())
+			{
+				ss >> cols;
+				//std::cout << "Correct Input." << std::endl;
+				exitCount++;
+			}
+			else if (incorrectEntry > 0)
+			{
+				std::cout << "Incorrect Entry." << std::endl;
+				std::cout << "Please enter an integer: No characters, symbols, doubles, floats, or decimals." << std::endl;
+				exitCount = 0;
+			}
+		}
+
+		if (cols >= 3 && cols <= 250)
+		{
+			exitCount = 0;
+		}
+		else
+		{
+			exitCount = 1;
+			std::cout << "Please use proper range." << std::endl;
+		}
+
+	} while(exitCount == 1);
+
+	return cols;
+}
+
+int promptAnts()        //Prompt user for number of ants (controlled by rows/cols)?
+{
+	int ants = 0;
+	int exitCount = 0;
+
+	//menu
+	std::cout << "Please enter the number of ants between 1 and 100." << std::endl;
+
+	do
+	{
+		exitCount = 0;			//sets to continue while loop in do-while
+		while (exitCount == 0)
+		{
+			int correctEntry = 0;
+			int incorrectEntry = 0;
+			std::string str = " ";
+			int inputLength = str.size();
+
+			//arrays to check string	
+			char numEntry[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+			
+			char letterEntry[] =
+			{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+			
+			char symEntry[] = {'`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+			'{', '}', '[', ']', '"', '<', '>', ',', '.', '?', '/', ' ', '-', '_', '+', '='};
+			//removed: '\', '|', ';', ':', ''',
+
+			//reads in user input
+			getline(std::cin, str);
+			std::stringstream ss(str);
+
+			//checks for empty strings
+			bool badEntry = false;
+			if (str.empty())
+			{
+				badEntry = true;
+			}
+
+			//checks for proper ints or incorrect entries
+			for (int t = 0; t < str.size(); t++)
+			{
+				for (int m = 0; m < 10; m++)
+				{
+					if (str[t] == numEntry[m])
+					{							
+						correctEntry ++;
+					}
+				}
+				for (int y = 0; y < 52; y++)
+				{
+					if (str[t] == letterEntry[y])
+					{										
+						incorrectEntry ++;
+					}
+				}
+				for (int n = 0; n < 28; n++)
+				{
+					if (str[t] == symEntry[n])
+					{
+						incorrectEntry ++;
+					}
+				}
+			}
+			if (badEntry == true)
+			{
+				std::cout << "Please enter at least one integer." << std::endl;
+				exitCount = 0;
+			}
+			else if (correctEntry == str.size())
+			{
+				ss >> ants;
+				//std::cout << "Correct Input." << std::endl;
+				exitCount++;
+			}
+			else if (incorrectEntry > 0)
+			{
+				std::cout << "Incorrect Entry." << std::endl;
+				std::cout << "Please enter an integer: No characters, symbols, doubles, floats, or decimals." << std::endl;
+				exitCount = 0;
+			}
+		}
+
+		if (ants >= 1 && ants <= 100)
+		{
+			exitCount = 0;
+		}
+		else
+		{
+			exitCount = 1;
+			std::cout << "Please use proper range." << std::endl;
+		}
+
+	} while(exitCount == 1);
+
+	return ants;
+}
+int promptDoodles()     //Prompt user for number of doodlebugs (controlled by rows/cols)
+{
+	int doodlebugs = 0;
+	int exitCount = 0;
+
+	//menu
+	std::cout << "Please enter the number of doodlebugs between 1 and 100." << std::endl;
+
+	do
+	{
+		exitCount = 0;			//sets to continue while loop in do-while
+		while (exitCount == 0)
+		{
+			int correctEntry = 0;
+			int incorrectEntry = 0;
+			std::string str = " ";
+			int inputLength = str.size();
+
+			//arrays to check string	
+			char numEntry[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+			
+			char letterEntry[] =
+			{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+			
+			char symEntry[] = {'`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+			'{', '}', '[', ']', '"', '<', '>', ',', '.', '?', '/', ' ', '-', '_', '+', '='};
+			//removed: '\', '|', ';', ':', ''',
+
+			//reads in user input
+			getline(std::cin, str);
+			std::stringstream ss(str);
+
+			//checks for empty strings
+			bool badEntry = false;
+			if (str.empty())
+			{
+				badEntry = true;
+			}
+
+			//checks for proper ints or incorrect entries
+			for (int t = 0; t < str.size(); t++)
+			{
+				for (int m = 0; m < 10; m++)
+				{
+					if (str[t] == numEntry[m])
+					{							
+						correctEntry ++;
+					}
+				}
+				for (int y = 0; y < 52; y++)
+				{
+					if (str[t] == letterEntry[y])
+					{										
+						incorrectEntry ++;
+					}
+				}
+				for (int n = 0; n < 28; n++)
+				{
+					if (str[t] == symEntry[n])
+					{
+						incorrectEntry ++;
+					}
+				}
+			}
+			if (badEntry == true)
+			{
+				std::cout << "Please enter at least one integer." << std::endl;
+				exitCount = 0;
+			}
+			else if (correctEntry == str.size())
+			{
+				ss >> doodlebugs;
+				//std::cout << "Correct Input." << std::endl;
+				exitCount++;
+			}
+			else if (incorrectEntry > 0)
+			{
+				std::cout << "Incorrect Entry." << std::endl;
+				std::cout << "Please enter an integer: No characters, symbols, doubles, floats, or decimals." << std::endl;
+				exitCount = 0;
+			}
+		}
+
+		if (doodlebugs >= 1 && doodlebugs <= 100)
+		{
+			exitCount = 0;
+		}
+		else
+		{
+			exitCount = 1;
+			std::cout << "Please use proper range." << std::endl;
+		}
+
+	} while(exitCount == 1);
+
+	return doodlebugs;
+}
+int promptSteps()       //Prompt user for number of steps
+{
+	int steps = 0;
+	int exitCount = 0;
+
+	//menu
+	std::cout << "Please enter the number of steps between 1 and 15000." << std::endl;
+
+	do
+	{
+		exitCount = 0;			//sets to continue while loop in do-while
+		while (exitCount == 0)
+		{
+			int correctEntry = 0;
+			int incorrectEntry = 0;
+			std::string str = " ";
+			int inputLength = str.size();
+
+			//arrays to check string	
+			char numEntry[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+			
+			char letterEntry[] =
+			{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+			
+			char symEntry[] = {'`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+			'{', '}', '[', ']', '"', '<', '>', ',', '.', '?', '/', ' ', '-', '_', '+', '='};
+			//removed: '\', '|', ';', ':', ''',
+
+			//reads in user input
+			getline(std::cin, str);
+			std::stringstream ss(str);
+
+			//checks for empty strings
+			bool badEntry = false;
+			if (str.empty())
+			{
+				badEntry = true;
+			}
+
+			//checks for proper ints or incorrect entries
+			for (int t = 0; t < str.size(); t++)
+			{
+				for (int m = 0; m < 10; m++)
+				{
+					if (str[t] == numEntry[m])
+					{							
+						correctEntry ++;
+					}
+				}
+				for (int y = 0; y < 52; y++)
+				{
+					if (str[t] == letterEntry[y])
+					{										
+						incorrectEntry ++;
+					}
+				}
+				for (int n = 0; n < 28; n++)
+				{
+					if (str[t] == symEntry[n])
+					{
+						incorrectEntry ++;
+					}
+				}
+			}
+			if (badEntry == true)
+			{
+				std::cout << "Please enter at least one integer." << std::endl;
+				exitCount = 0;
+			}
+			else if (correctEntry == str.size())
+			{
+				ss >> steps;
+				//std::cout << "Correct Input." << std::endl;
+				exitCount++;
+			}
+			else if (incorrectEntry > 0)
+			{
+				std::cout << "Incorrect Entry." << std::endl;
+				std::cout << "Please enter an integer: No characters, symbols, doubles, floats, or decimals." << std::endl;
+				exitCount = 0;
+			}
+		}
+
+		if (steps >= 1 && steps <= 15000)
+		{
+			exitCount = 0;
+		}
+		else
+		{
+			exitCount = 1;
+			std::cout << "Please use proper range." << std::endl;
+		}
+
+	} while(exitCount == 1);
+
+	return steps;
+}
