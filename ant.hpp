a0bce5ec7b6d07b35c564a05f89a1dd02e5a60cc
Rondellio <ron.walters23@gmail.com>
2018-02-12 16:45:30 -0500

    2/12/18 4:45ET update

--- ant.hpp
+++ ant.hpp
@@ -1,13 +1,13 @@
 #ifndef ANT_HPP
 #define ANT_HPP
-#include "critter.hpp"
+#include "Critter.hpp"
 
 class Ant: public Critter
 {
 	public:
 		Ant(int, int, int);
-		virtual void move(Critter**, int, int);
-		virtual void breed(Critter**, int, int);
+		virtual void move(Critter***, int, int);
+		virtual void breed(Critter***, int, int);
 };
 
 #endif
