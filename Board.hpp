ce453c375ad3e5c7ea53bb7665a7d78169963b9c
Rondellio <ron.walters23@gmail.com>
2018-02-15 19:52:16 -0500

    Updated 2/15/18 - Ron

--- Board.hpp
+++ Board.hpp
@@ -1,30 +1,31 @@
-Board Header File:
+//Board Header File:
 #ifndef BOARD_HPP
 #define BOARD_HPP
-"Critter.hpp"
+#include "Critter.hpp"
+#include "Ant.hpp"
+#include "Doodlebug.hpp"
+#include <stdlib.h>
 
-Class Board
+class Board
 {
-	Private:
+	private:
 		//(if we want extra credit, use these)
-		int rows = 0;
-		int cols = 0;
-		const int boardRows = 20;
-		const int boardCols = 20;
+		int rows;
+		int cols;	
 		//char **board;	// needs to be a critter
 		Critter ***board;
-	Public:
-    Board();  //default
-    Board(int, int);  //overloard
-    ~Board();  //destructor
-		Void createBoard();
-		Void printBoard();
+	public:
+		Board();  //default
+		Board(int, int);  //overloard
+		~Board();  //destructor
+		void createBoard();
+		void printBoard();
 		//(extra credit)
-		Void setRows(int r);	//could use my inputval function for these -DG
-		Void setColumns(int c);
+		void setRows(int r);	//could use my inputval function for these -DG
+		void setColumns(int c);
 
 		//gonna need this at some point?
-		int getRandom(int min, int max);
+	//	int getRandom(int min, int max);
 };
 
 #endif
