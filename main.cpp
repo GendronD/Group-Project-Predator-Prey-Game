b3b5707fd46b006e520503aba79679d8ac95e0b8
Devin <gendron.devin@gmail.com>
2018-02-18 10:45:24 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,9 +23,13 @@ int main ()
     //random seed
     srand(time(NULL));
 
-    int choice = 0;         //menu choice
-    Board *gameboard;       //game board object
+    int choice = 0;
+    Board *gameboard;
     int steps = 0;
+    int rows = 0;
+    int cols = 0;
+    int ants = 0;
+    int dbugs = 0;
 
     //extra credit message
     cout << "Extra credit is implemented on this project\n" << endl;
@@ -34,41 +38,45 @@ int main ()
     cout << "Predator Prey Menu" << endl;
     cout << "   1. Play Set Game" << endl;
     cout << "   2. Play Custom Game (EC)" << endl;
+    cout << "   3. Quit Game." << endl;
     cout << "Please choose an option: " << endl;
 
     //validate the user's selection
-    choice = inputValidation();
+    choice = mainMenu();
 
     switch (choice)
     {
-        case 1:     {//make a default gameboard;
+        case 1:  //make a default gameboard;
             gameboard = new Board;
             steps = promptSteps();
-        }
             break;
 
-        case 2:     {//make a custom gameboard
-            int rows = promptRows();        //Prompt user for number of rows
-            int cols = promptCols();        //Prompt user for number of columns
-            int ants = promptAnts();        //Prompt user for number of ants
-            int dbugs = promptDoodles();     //Prompt user for number of doodlebugs
+        case 2:  //make a custom gameboard
+            rows = promptRows();        //Prompt user for number of rows
+            cols = promptCols();        //Prompt user for number of columns
+            ants = promptAnts();        //Prompt user for number of ants (controlled by rows/cols)
+            dbugs = promptDoodles();     //Prompt user for number of doodlebugs (controlled by rows/cols)
             steps = promptSteps();       //Prompt user for number of steps
             //constructor with parameters for gameboard
             gameboard = new Board(rows, cols, ants, dbugs);
-        }
+
+            cout << "Initial gameboard. Doodlebugs, start your engines." << endl;
+            gameboard->printBoard();
+
+            //runs simulation
+            gameboard->simulation(steps);
+
+            std::cout<< "game ended!" << std::endl;
             break;
+
+        case 3 : //quit game
+            cout << "Game Quit" << endl;
+            break;
+
         default:    cout << "That's not a menu option. Please try again!\n" << endl;
             //start over by recursively calling main
             main();
     }
-
-    cout << "Initial gameboard. Doodlebugs, start your engines." << endl;
-    gameboard->printBoard();    //prints gameboard
-
-    //runs simulation
-    gameboard->simulation(steps);   //runs predator prey simulation
-
-    std::cout<< "game ended!" << std::endl;
 
     return 0;
 }
