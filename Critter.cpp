ae0ad85a5a38d19e3233f6d7df371270a1f4daca
Elizabeth Donato <donatoe@oregonstate.edu>
2018-02-18 07:51:00 -0600

    Create Critter.cpp

---
+++ Critter.cpp
@@ -0,0 +1,38 @@
+/*********************************************************************
+ * Program name: Group Project 1 - Group 8 - Critter.cpp
+ * Authors: Sheila Babayan, Becky Chao, Elizabeth Donato, 
+ * Devin Gendron, Ronald Walters
+ * Date: 2/19/2018
+ * Description: This is the implementation file for our Critter class. It 
+ * contains the functions for Critter objects.
+ **********************************************************************/
+#include "Critter.hpp"
+
+Critter::Critter()
+{
+	daysAlive = 0;
+	symbol = ' ';
+}
+
+char Critter::getSymbol()
+{
+	return symbol;
+}
+
+void Critter::move(Critter*** board, int row, int col)
+{
+	symbol = ' ';
+}
+
+void Critter::breed(Critter*** board, int row, int col)
+{
+	symbol = ' ';
+}
+
+void Critter::starve(Critter***)
+{
+
+}
+
+Critter::~Critter()
+{}
